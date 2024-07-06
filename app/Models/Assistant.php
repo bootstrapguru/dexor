f645e6eb537ca0f5945f6c9097f099473053b7c4
Vijay Tupakula <vijay@getcate.ai>
2024-07-06 09:22:54 -0500

    Add models for Assistant, Thread, and Message based on migrations

---
+++ app/Models/Assistant.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Assistant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'model',
+        'description',
+        'prompt',
+        'tools'
+    ];
+
+    protected $casts = [
+        'tools' => 'array'
+    ];
+
+    public function threads()
+    {
+        return $this->hasMany(Thread::class);
+    }
+}
